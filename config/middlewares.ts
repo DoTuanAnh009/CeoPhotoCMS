d1d4da1ad5c71b7128dbf8c41b7d2b8aee8f3959
DoTuanAnh009 <anhdt@ichiba.vn>
2026-03-29 11:44:34 +0700

    update claoude

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,24 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "res.cloudinary.com",
+          ],
+        },
+      },
+    },
+  },
+
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
